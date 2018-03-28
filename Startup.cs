96dfeefa3aa5d6d74ee1b38376dcf07e8d663176
SriDharmaDev <37127910+SriDharmaDev@users.noreply.github.com>
2018-03-27 19:48:20 -0500

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using System.Threading.Tasks;
-using DutchTreat.Data;
+
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using DutchTreat.Data;
+using Microsoft.AspNetCore.Identity;
+using DutchTreat.Data.Entities;
 
 namespace DutchTreat
 {
@@ -25,15 +30,32 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<StoreUser, IdentityRole>(cfg=>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                
+            })
+            .AddEntityFrameworkStores<DutchContext>();
+
+
+
             services.AddDbContext<DutchContext>(cfg =>
             {
                 cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
             });
+            
+
+            services.AddAutoMapper();
 
             services.AddTransient<IMailService, NullMailService>();
             // Support for real mail service.
 
-            services.AddMvc();
+            services.AddTransient<DutchSeeder>();
+
+            services.AddScoped<IDutchRepository, DutchRepository>();
+
+            services.AddMvc()
+                .AddJsonOptions(opt =>opt.SerializerSettings.ReferenceLoopHandling= ReferenceLoopHandling.Ignore);
 
         }
 
@@ -51,6 +73,7 @@ namespace DutchTreat
 
             app.UseStaticFiles();
 
+            app.UseAuthentication();
 
             app.UseMvc(cfg =>
             {
@@ -59,7 +82,15 @@ namespace DutchTreat
                     new { Controller = "App", Action = "Index" });
             });
 
-
+            if(env.IsDevelopment())
+            {
+                //Seed the database
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+                    seeder.Seed();         // not able to add wait method but shawn adds succesfully.
+                }
+            }
         }
     }
 }
